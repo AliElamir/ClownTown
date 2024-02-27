341da57945528057d1b75727e0dcd1de62c91237
AliElamir <ali.elamiir.a@gmail.com>
2024-02-27 09:42:35 +0300

    add componenets

--- src/components/Clown.jsx
+++ src/components/Clown.jsx
@@ -4,11 +4,16 @@ import axios from 'axios'
 const Clown = () => {
   const [clowns, setClowns] = useState([])
 
-  useEffect(() => {
-    const getClowns = async () => {
-      // const response = // get information from the database
-      // setClowns(...)
+  const getClowns = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/clowns')
+      setClowns(response)
+      console.log(response)
+    } catch (error) {
+      console.log(error)
     }
+  }
+  useEffect(() => {
     getClowns()
   }, [])
 
@@ -17,7 +22,7 @@ const Clown = () => {
       <h1 className="headers">Clowny clown</h1>
       <section className="container-grid">
         {clowns.map((clown) => (
-          <div key={clowns.id}>
+          <div key={clowns._id}>
             <h3>{clown.name}</h3>
             <img src={clown.image} alt={clown.name} />
           </div>
